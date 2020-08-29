08a572b05ee08272063cc1a561814a698dbeb659
Gabriel Brun <lydstyl@gmail.com>
2020-08-29 17:34:51 +0200

    CRUD

--- app.js
+++ app.js
@@ -49,4 +49,98 @@ app.get('/createpoststable', (req, res) => {
   })
 })
 
+// Insert post 1
+app.get('/addpost1', (req, res) => {
+  const post = { title: 'Post One', body: 'This is post number one' }
+
+  const sql = 'INSERT INTO posts SET?'
+
+  db.query(sql, post, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(result)
+    res.send('Post 1 added ...')
+  })
+})
+
+// Insert post 2
+app.get('/addpost2', (req, res) => {
+  const post = { title: 'Post Two', body: 'This is post number two' }
+
+  const sql = 'INSERT INTO posts SET?'
+
+  db.query(sql, post, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(result)
+    res.send('Post 2 added ...')
+  })
+})
+
+// Select posts
+app.get('/getposts', (req, res) => {
+  const sql = 'SELECT * FROM posts'
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(result)
+
+    res.send('Posts fetched ...')
+  })
+})
+
+// Select single post
+app.get('/getpost/:id', (req, res) => {
+  const sql = `SELECT * FROM posts WHERE id = ${req.params.id}`
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('result', result)
+
+    res.send('Post fetched ...')
+  })
+})
+
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+  const newTitle = 'Updated title'
+
+  const sql = `UPDATE posts SET title='${newTitle}' WHERE id = ${req.params.id}`
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('result', result)
+
+    res.send('Post updated ...')
+  })
+})
+
+// Delete post
+app.get('/deletepost/:id', (req, res) => {
+  const sql = `DELETE FROM posts WHERE id = ${req.params.id}`
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('result', result)
+
+    res.send('Post deleted ...')
+  })
+})
+
 app.listen('3000', () => console.log('Server started on port 3000'))
